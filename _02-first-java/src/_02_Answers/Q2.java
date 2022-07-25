83522d86c4866f5718be698fdfc471ff5bc3d0a6
Baesil <baesilshowcath@gmail.com>
2022-07-26 04:04:58 +0530

    doc-added Q2✍🏻answer

--- _02-first-java/src/_02_Answers/Q2.java
+++ _02-first-java/src/_02_Answers/Q2.java
@@ -1,7 +1,12 @@
 package _02_Answers;
 
+import java.util.Scanner;
+
 public class Q2 {
     public static void main(String[] args) {
-        sout
+        System.out.println("What's your name ? ");
+        Scanner in = new Scanner(System.in);
+        String name = in.nextLine();
+        System.out.println("Hi "+name+"!!"+" Welcome to the DSA Bootcamp! Let's Build together");
     }
 }
