27a5bf208a5b64756daf23ecc60d8d04b06f7ff0
Baesil <baesilshowcath@gmail.com>
2022-07-26 03:07:11 +0530

    doc- added Q1✍🏻 answer

--- _02-first-java/src/_02_Answers/Q1.java
+++ _02-first-java/src/_02_Answers/Q1.java
@@ -7,7 +7,16 @@ import java.util.Scanner;
 public class Q1 {
     public static void main(String[] args) {
         System.out.println("Enter a number , I will find whether it is odd or even");
+        //input
         Scanner in= new Scanner(System.in);
+        int num = in.nextInt();
+        //processing
+        if (num % 2 == 0){
+            System.out.println("The number you entered is 'even'");
+        }
+        else {
+            System.out.println("The number you entered is 'odd'");
+        }
 
     }
 }
