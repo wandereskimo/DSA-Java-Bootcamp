d5b9ffdcd3f73f52ed8196bede15d6c708391cef
Baesil <baesilshowcath@gmail.com>
2022-07-26 02:10:13 +0530

    updated the README.md file
    Added information and details about this repo in README.md file

---
+++ _02-first-java/src/_02_Answers/Q1.java
@@ -0,0 +1,13 @@
+package _02_Answers;
+
+//1,Write a program to print whether a number is even or odd, also take input from the user.
+
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args) {
+        System.out.println("Enter a number , I will find whether it is odd or even");
+        Scanner in= new Scanner(System.in);
+
+    }
+}
